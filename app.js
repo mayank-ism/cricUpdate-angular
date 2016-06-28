17cc5654b11fbfd2a3afacd9c86dcf8ce7e28977
Mayank Subhash Yadav <mayank-ism@Mayanks-MacBook-Pro-2.local>
2016-06-28 18:35:02 +0530

    added logging in controllers

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ var express = require('express');
 
 var app = express();
 var config = require('./config/config');
+var log4js = require('log4js');
+
+log4js.configure({
+  appenders: [
+    { type: 'console' },
+    { type: 'file', filename: __dirname + '/log/app.log', category: 'app' }
+  ]
+});
+
+var logger = log4js.getLogger('app.js');
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/view/index.html');
+  res.sendFile(__dirname + '/view/index.html');
 });
 
 app.use(require('./controller'));
 
 app.listen(config.port);
+logger.info("Listening to port: " + config.port);
--- controller/news.js
+++ controller/news.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var request = require('request')
 var config = require(__dirname + '/..' + '/config/config');
+
+var log4js = require('log4js');
+
+log4js.configure({
+  appenders: [
+    { type: 'console' },
+    { type: 'file', filename: __dirname + '/../' + 'log/news.log', category: 'news' }
+  ]
+});
+
+var logger = log4js.getLogger('news.js');
+logger.info("In news controller");
+
+
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	// if(req.xhr) {
-		var request_url = config.api_url + "cricketNews";
-		request(request_url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    });	
-	// } else {
-	// 	res.send("Ain't your territory");
-	// }
+  var request_url = config.api_url + "cricketNews";
+  logger.info("Calling " + request_url);
+  request(request_url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      logger.info("SUCCESS: Got response from " + request_url);
+      logger.debug("Response: " + body);
+      res.send(body);
+    } else {
+      logger.error("Request failed for " + request_url);
+      logger.error("Error: " + error + " Response Status: " + response.statusCode);
+    }
+  });	
 });
 
 module.exports = router;
